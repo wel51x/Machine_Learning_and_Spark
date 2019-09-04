b0ed56ac00e05836fe81fe93aa6e06d6048e5f38
Winston <faluovo@gmail.com>
2019-09-04 02:07:48 -0400

    Add files via upload

--- Chapter_4/Ex4d.1.py
+++ Chapter_4/Ex4d.1.py
@@ -1,14 +1,10 @@
 # derived from EX4a.1
 import pyspark
-from pyspark.sql.functions import round
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.regression import LinearRegression
-from pyspark.ml import Pipeline
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
-from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.classification import DecisionTreeClassifier, GBTClassifier
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
+import pandas as pd
 
 # Creating a SparkSession
 spark = SparkSession.builder \
@@ -38,14 +34,18 @@ flights = flights.withColumn('label', (flights.delay >= 15).cast('integer'))
 
 flites = flights.select('mon', 'depart', 'duration', 'label')
 
-print("Sample model input")
-print(flites.toPandas().sample(12))
-
 # Create 'features' vector: 'km', 'org_dummy', 'dow_dummy'
 assembler = VectorAssembler(inputCols=['mon', 'depart', 'duration'], outputCol='features')
 
 # Consolidate predictor columns
-flites = assembler.transform(flights)
+flites = assembler.transform(flites)
+
+pd.set_option('display.max_columns', None) # all cols
+pd.set_option('display.width', 161)
+pd.set_option('display.max_colwidth', 199)
+
+print("Sample model input")
+print(flites.toPandas().sample(12))
 
 # Split the data into training and testing sets
 flights_train, flights_test = flites.randomSplit([0.8, 0.2], seed=23)
@@ -77,32 +77,38 @@ print(gbt.featureImportances)
 
 spark.stop()
 '''
-DecisionTreeClassificationModel (uid=DecisionTreeClassifier_f233574f0321) of depth 5 with 25 nodes
-  If (feature 1 <= 9.51)
-   If (feature 2 <= 118.5)
+DecisionTreeClassificationModel (uid=DecisionTreeClassifier_a6d8645d598e) of depth 5 with 29 nodes
+  If (feature 1 <= 9.524999999999999)
+   If (feature 2 <= 119.5)
     Predict: 0.0
-   Else (feature 2 > 118.5)
+   Else (feature 2 > 119.5)
     If (feature 0 <= 6.5)
-     Predict: 0.0
+     If (feature 1 <= 7.74)
+      Predict: 0.0
+     Else (feature 1 > 7.74)
+      If (feature 0 <= 4.5)
+       Predict: 0.0
+      Else (feature 0 > 4.5)
+       Predict: 1.0
     Else (feature 0 > 6.5)
      If (feature 0 <= 10.5)
       Predict: 0.0
      Else (feature 0 > 10.5)
-      If (feature 1 <= 8.575)
+      If (feature 1 <= 8.59)
        Predict: 0.0
-      Else (feature 1 > 8.575)
+      Else (feature 1 > 8.59)
        Predict: 1.0
-  Else (feature 1 > 9.51)
+  Else (feature 1 > 9.524999999999999)
    If (feature 0 <= 6.5)
-    If (feature 2 <= 85.5)
-     If (feature 2 <= 49.5)
+    If (feature 2 <= 125.5)
+     If (feature 2 <= 50.5)
       Predict: 0.0
-     Else (feature 2 > 49.5)
-      If (feature 0 <= 1.5)
-       Predict: 1.0
-      Else (feature 0 > 1.5)
+     Else (feature 2 > 50.5)
+      If (feature 1 <= 13.425)
        Predict: 0.0
-    Else (feature 2 > 85.5)
+      Else (feature 1 > 13.425)
+       Predict: 1.0
+    Else (feature 2 > 125.5)
      Predict: 1.0
    Else (feature 0 > 6.5)
     If (feature 0 <= 10.5)
@@ -118,26 +124,26 @@ DecisionTreeClassificationModel (uid=DecisionTreeClassifier_f233574f0321) of dep
 
 
 GBTClassifier trees:
-  DecisionTreeRegressionModel (uid=dtr_533c02f10bda) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_7efa156fe252) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_7cf20e0c20d9) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_16ae23b5f16c) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_49e5065484bd) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_a64c7391608a) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_70b8fb8c5fba) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_fba2bfa015fe) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_8f059b176a23) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_4e2bbd10a134) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_58c117964c8d) of depth 5 with 61 nodes
-  DecisionTreeRegressionModel (uid=dtr_13ebf5c1d7eb) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_09792b3b467f) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_de4e775cb2be) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_59caf2357af1) of depth 5 with 61 nodes
-  DecisionTreeRegressionModel (uid=dtr_3ec9780243eb) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_39ca4a1698d9) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_796f58b035b6) of depth 5 with 61 nodes
-  DecisionTreeRegressionModel (uid=dtr_dbb13ed46192) of depth 5 with 63 nodes
-  DecisionTreeRegressionModel (uid=dtr_5e1e9e2d3553) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_9719243683f8) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_dc01c669ecef) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_44ada9034bb7) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_993ccc594474) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_67380a052abe) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_cffc5fcf5cb9) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_d75aa1c8742d) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_4e74be8bcb14) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_99831d64c136) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_f16ca0c65b1e) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_eb8368dd3eb6) of depth 5 with 61 nodes
+  DecisionTreeRegressionModel (uid=dtr_5575d25f59c3) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_bc9af10f2078) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_83dbb602ee08) of depth 5 with 61 nodes
+  DecisionTreeRegressionModel (uid=dtr_96c0923c2784) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_d052e42b6847) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_278debbd0005) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_9b3bc7c606f8) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_a262423f1e37) of depth 5 with 63 nodes
+  DecisionTreeRegressionModel (uid=dtr_f2433aa1ef0c) of depth 5 with 63 nodes
 
-(3,[0,1,2],[0.332295623201302,0.2837268590362885,0.3839775177624095])
+(3,[0,1,2],[0.28006563889404096,0.3193787093092136,0.40055565179674546])
 '''
